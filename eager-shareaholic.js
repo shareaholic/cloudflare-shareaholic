0b835e10472c7d8324e7fb266b939f442c5b480f
jmeattle <jmeattle@shareaholic.com>
2016-01-13 20:41:43 -0500

    Initial commit

---
+++ eager-shareaholic.js
@@ -0,0 +1,19 @@
+(function() {
+  var options = INSTALL_OPTIONS;
+  if (!options.siteId) {
+		return;
+	}
+  
+  var shr = document.createElement('script');
+    shr.setAttribute('data-cfasync', 'false');
+    shr.src = '//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js';
+    shr.type = 'text/javascript'; shr.async = 'true';
+    shr.onload = shr.onreadystatechange = function() {
+      var rs = this.readyState;
+      if (rs && rs != 'complete' && rs != 'loaded') return;
+      var site_id = options.siteId;
+      try { Shareaholic.init(site_id); } catch (e) {}
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(shr, s);
+})();
