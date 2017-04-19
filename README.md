ab81c74f0df840ecc5ebd4a0f0e92065d38d8fe2
Saša Jovanić <sasa@simplify.ba>
2017-04-19 14:31:49 +0200

    PUB-1135 - Cloudflare app with OAuth

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Shareaholic for Eager.io
+# Shareaholic for Cloudflare (Eager.io)
 
-<a href="https://eager.io/app/shareaholic/install?source=button">
-  <img src="https://install.eager.io/install-button.png" border="0" width="126">
-</a>
+[![Install](https://install.eager.io/install-button.png)](https://www.cloudflare.com/apps/shareaholic/install?source=button)
 
-##### [About Shareaholic](https://shareaholic.com/publishers) &nbsp; &nbsp; [Shareaholic on Eager.io](https://eager.io/app/shareaholic)
+##### [About Shareaholic](https://shareaholic.com/about)
 
---
+++ cloudflare-shareaholic.js
@@ -0,0 +1,41 @@
+(function() {
+  var options = INSTALL_OPTIONS;
+  var isPreview = INSTALL_ID == "preview";
+
+  if (isPreview) {
+    if (!options.siteId) {
+      options.siteId = 'd66d1e26c0f1f07ba8e3709578a8e11a';
+    }
+  }
+  if (!options.siteId) { return; }
+
+  var counterEl;
+  var render = function(){
+    counterEl = Eager.createElement(options.shareCounter, counterEl);
+    if (options.showShareCounter){
+      counterEl.innerHTML = "<div class='shareaholic-canvas' data-app='total_share_count'></div>";
+    }
+  };
+
+  var setOptions = function(opts){
+    options = opts;
+    render();
+  };
+
+  var shr = document.createElement('script');
+  shr.setAttribute('data-cfasync', 'false');
+  shr.src = '//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js';
+  shr.type = 'text/javascript'; shr.async = 'true';
+  shr.onload = shr.onreadystatechange = function() {
+    var rs = this.readyState;
+    if (rs && rs != 'complete' && rs != 'loaded') return;
+    render();
+    try { Shareaholic.init(options.siteId); } catch (e) {}
+  };
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(shr, s);
+
+  INSTALL_SCOPE = {
+    setOptions: setOptions
+  }
+})();
